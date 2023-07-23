fbb9a4a6dff6fd0e591cfc4661a63d82132455c4
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-07-23 16:54:37 +0200

    added token verification

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -11,7 +11,7 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   try {
-    if (bearer !== "Bearer") {
+    if (bearer !== "Bearer" && !token) {
       next(HttpError(401));
     }
     const { id } = jwt.verify(token, SECRET_KEY);
